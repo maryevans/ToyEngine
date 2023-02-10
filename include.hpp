520b52a48a82bf61871b5cba538ba6c4d5918f1a
Zea Jenson <zeajenson@gmail.com>
2023-02-10 00:00:48 -0700

    Innitial settup

---
+++ include.hpp
@@ -0,0 +1,11 @@
+
+#define VULKAN_HPP_NO_CONSTRUCTORS
+#include "vulkan/vulkan.hpp"
+#include "vulkan/vulkan_raii.hpp"
+#include "GLFW/glfw3.h"
+#include "fmt/format.h"
+#include "spdlog/spdlog.h"
+
+#include <thread>
+#include <iostream>
+#include <chrono>
